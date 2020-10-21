5ee65fac682d157f7042fed1b086e5e12bd482cf
Lippo <lippo997@gmail.com>
2020-10-21 20:49:08 +0200

    Add margin bottom to title

--- web/header.tsx
+++ web/header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
     return (
         <div>
-            <h1 className="display-1">Among us Ranking</h1>
+            <h1 id="title" className="display-1">Among us Ranking</h1>
         </div>
     )
 }
