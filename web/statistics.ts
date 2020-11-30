aa5bd3a231c3f4d20eedc495a8ef36b107e9bb22
Lippo <filippo.nardini@studio.unibo.it>
2020-11-30 01:34:30 +0100

    Small refactoring

--- web/statistics.ts
+++ web/statistics.ts
@@ -4,7 +4,7 @@ const DEFAULT_CONFIDENCE: number = 0.95
 const memoizedZ: number = stats.probit(1-(1-DEFAULT_CONFIDENCE)/2);
 
 // https://medium.com/@gattermeier/calculating-better-rating-scores-for-things-voted-on-7fa3f632c79d
-export function lower_bound_wilson(upvotes: number, n: number = 0, confidence: number = 0.95): number {
+export function lower_bound_wilson(upvotes: number, n: number = 0, confidence: number = DEFAULT_CONFIDENCE): number {
   if (n === 0) return 0
 
   // for performance purposes you might consider memoize the calcuation for z
