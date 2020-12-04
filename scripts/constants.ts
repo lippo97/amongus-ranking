8d1def919154910227a6b1c10bf33e6cef0228cd
Lippo <filippo.nardini@studio.unibo.it>
2020-12-04 13:09:49 +0100

    Fix bug

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -5,4 +5,4 @@ const MQTT_HOST = process.env.HOST || 'localhost';
 const MQTT_PORT = parseInt(process.env.PORT || '1883');
 export const MQTT_URI = `${MQTT_PROCOL}://${MQTT_HOST}:${MQTT_PORT}`;
 export const DATA_DIR = 'data';
-export const RANKING_FILE = '${DATA_DIR}/ranking.json';
+export const RANKING_FILE = `${DATA_DIR}/ranking.json`;
